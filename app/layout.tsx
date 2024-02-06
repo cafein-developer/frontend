9f5c6e9f303337b0807073fea2e9cd6baf976170
김은식 <gamrom@MacBook-Pro.local>
2024-02-07 03:47:55 +0900

    routing 세팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="min-w-[390px] max-w-[500px]">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -34,8 +34,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      api 테스트
-      {data}
+      메인입니다
     </main>
   )
 }
